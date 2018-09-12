1385285b1b4e5cfcea98ce38c14157cbb2c66d21
vvertash <vvertash@gmail.com>
2018-09-12 23:56:49 +0300

    week 4 added

--- week4/ex3.c
+++ week4/ex3.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void type_prompt() {
-	printf("> ");
-}
-void read_command(char *command) {
-	scanf("%s", command);
-}
 int main(void) {
 	char command[10];
-	while (TRUE) {
-		type_prompt();
-		read_command(command);
+	while (1) {
+		scanf("%s", command);
 		system(command);
 	}
 	return EXIT_SUCCESS;
